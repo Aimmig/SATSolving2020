90579b53c7315455d5be9350f69a569b3ea17978
Andre Immig <andre.immig@student.kit.edu>
2020-05-01 00:10:37 +0200

    Add some comments to ex3 sheet1

--- sheet1/ex3.cpp
+++ sheet1/ex3.cpp
@@ -1,3 +1,24 @@
+/*
+ Calculate number of variables and clauses for
+ checking wether W(2,k) > n
+
+ Using the encoding of the lecture we have:
+ - for each integer 1,...n a variable x_i
+ - some clauses to exclude all possibility of k equally spaced
+
+ So trivally we have just n variables
+
+ Calculating the clauses is a bit tricky
+ Dividing the clauses in subset S_r, such that each subset
+ exludes just the possibility spaced r appart
+
+ 1,2,.......,k	   1,3,..,1+r*k
+ 2,3,......,k+1    ....
+ .............	   ....
+ n-(k-1),....,n	   n-(k-1)*2,...,n    n-(k-1)*r
+*/
+
+
 extern "C" {
     #include "ipasir.h"
 }
@@ -8,20 +29,25 @@ extern "C" {
 #include <ctype.h>
 #include <vector>
 
-
+/* The upper for equally spacing k from n */
 int upperBound(int n, int k){
 	return (n-1)/(k-1);
 }
 
-
+/*
+ * Add all clauses that exclude the equally spaced items
+ * with distant r
+ */
 int addappartClauses(void* solver, int n, int k, int r){
 	int res = 0;
+	//biggest starting index of variables for clauses
 	int maxInternal = n-(k-1)*r;
 	std::vector<int> variables;
 	for (int val = 1; val<=maxInternal; val++){
 		for (int v = 0; v < k; v++){
 			variables.push_back(val+v*r);
 		}
+		//add these claues positive & negative
 		res+=2;
 		for (auto l : variables){
 			ipasir_add(solver, l);
@@ -38,6 +64,9 @@ int addappartClauses(void* solver, int n, int k, int r){
 	return res;
 }
 
+/*
+ * Adds all clauses for encoding the problem
+ */
 void loadFormula(void* solver, int n, int k, int outVariables[]) {
 	int maxR = upperBound(n,k);
 	for (int r=1; r<=maxR; r++){
