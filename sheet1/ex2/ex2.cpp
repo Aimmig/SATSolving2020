2c34ed8b24238238ba2b44a2e8e4533d2be888e3
Andre Immig <andre.immig@student.kit.edu>
2020-05-03 12:05:33 +0200

    Add the clauses to ipasir solver

--- sheet1/ex2/ex2.cpp
+++ sheet1/ex2/ex2.cpp
@@ -21,14 +21,17 @@ int encode_block(int u, int j, int b, int i, int k,int n, int s){
 
 void addBlockClauses(void* solver, int num, int k){
 	int n =k*k;
+	int var = 0;
 	for (int s=0;s<k;s++){
 		for (int b=0;b<k;b++){
 			for(int j=0; j<k;j++){
 				for (int u=0; u<k; u++){
-					std::cout<<encode_block(u,j,b,num,k,n,s)<<" ";
-					//std::cout<<1+u+(j)*n+b*k+n*n*(i)+s*n*k<<" ";
+					var = encode_block(u,j,b,num,k,n,s);
+					std::cout<<var<<" ";
+					ipasir_add(solver,var);
 				}
 			}
+			ipasir_add(solver,0);
 			std::cout<<std::endl;
 		}
 	}
@@ -42,13 +45,21 @@ void addRules(void* solver, int k){
 	int n=k*k;
 	for (int i=0; i<n;i++){
 		for (int r=0; r<n; r++){
+			//clause for rows
 			for (int c=0; c<n; c++){
-				std::cout<<encode(n,r,c,i)<<" ";
+				int var = encode(n,r,c,i);
+				std::cout<<var<<" ";
+				ipasir_add(solver,var);
 			}
+			ipasir_add(solver,0);
 			std::cout<<std::endl;
+			//clauses for colums
 			for (int c=0; c<n; c++){
-				std::cout<<encode(n,c,r,i)<<" ";
+				int var = encode(n,c,r,i);
+				std::cout<<var<<" ";
+				ipasir_add(solver,var);
 			}
+			ipasir_add(solver,0);
 			std::cout<<std::endl;
 		}
 		std::cout<<"----"<<std::endl;
@@ -93,26 +104,6 @@ int betterFile(void* solver, const char* filename, int* outVariables){
 		}
 		std::cout<<std::endl;
 	}
-	/*
-		while(iss >> part){
-			splitted.push_back(part);
-		}
-		if (splitted[0].compare(std::string("p")) == 0){
-			numVertices = std::stoi(splitted[2]);
-			//numEdges = std::stoi(splitted[3]);
-		}
-		if (splitted[0].compare(std::string("e")) == 0){
-			std::pair<int,int> edge;
-			int v1 = std::stoi(splitted[1]);
-			int v2 = std::stoi(splitted[2]);
-			edge.first = v1;
-			edge.second = v2;
-			res.push_back(edge);
-		}
-		splitted.clear();
-	}
-	*outVariables = numVertices;
-	*/
 	return 0;
 }
 
@@ -125,9 +116,8 @@ int main(int argc, char **argv) {
 	}
 
 	void *solver = ipasir_init();
-	int numVertices = 0;
-	//int edgelist = 
-	betterFile(solver, argv[1], &numVertices);
+	int k = 0;
+	betterFile(solver, argv[1], &k);
 	
 	/*if (!loaded) {
 		std::cout << "c The input formula " << argv[1] << " could not be loaded." << std::endl;
@@ -136,49 +126,26 @@ int main(int argc, char **argv) {
 	else {
 		std::cout << "c Loaded, solving" << std::endl;
 	}*/
-	/*
-	enforceColor(solver,colors,numVertices);
 
+	std::cout<<"Now solving sudo"<<std::endl;
 	int satRes = ipasir_solve(solver);
 
 	
-	while (satRes == 20) {
-		//std::cout<<"c--------"<<std::endl;
-		std::cout<<"c The graph is not colorable with "<<colors<<" colors" << std::endl;
-		std::cout<<"c Now trying with "<<colors+1<<" colors"<<std::endl;
-		//std::cout<<"c--------"<<std::endl;
-		addMoreVertexClauses(solver, colors, numVertices, edgelist);
-		colors++;
-		addClauses(solver,numVertices,colors);
-
-		for (int col = minColor; col <colors; col++){
-			for (int v=0; v<numVertices;v++){
-				int activationLit = getActivationLiteral(v, col,numVertices);
-				//std::cout<<"Setting Activation literal"<<activationLit<<std::endl;
-				ipasir_assume(solver,activationLit);
-			}
-		}
-		enforceColor(solver,colors,numVertices);
-		satRes = ipasir_solve(solver);
+	if (satRes == 20) {
+		std::cout<<"Invalid sudo, not solvable"<<std::endl;
 	}
 	
 	if (satRes == 10) {
-		std::cout << "c The graph is colorable with " <<colors<<" colors"<< std::endl;
-		std::cout << "c The output format is: <node> <color>"<< std::endl;
-		int vertex = 0;
-		int color = 0;
-		int variables = getNumVariables(numVertices,colors);
-		int value = 0;
-		
+		std::cout<<"Solutin for sudoku found"<<std::endl;
 		//only request values for even variables (odd are activations literals)
-		for (int var = 2; var <= variables; var+=2) {
+		/*for (int var = 2; var <= variables; var+=2) {
 			value = ipasir_val(solver, var);
 			if (value > 0){
 				vertex = decode_vertex(value,numVertices);
 				color = decode_col(value,numVertices);
 				std::cout <<"v "<< vertex <<" "<< color<<std::endl;
 			}
-		}
-	}*/
+		}*/
+	}
 	return 0;
 }
