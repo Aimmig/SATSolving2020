4f81cc02b8199045342be7b896574a60aaf91a94
Andre Immig <andre.immig@student.kit.edu>
2020-05-09 09:49:35 +0200

    Minor optimizations in pythagoren tripplets coloring

--- sheet1/ex4/ex4.cpp
+++ sheet1/ex4/ex4.cpp
@@ -1,17 +1,35 @@
 /*
+ * Generates pythagorean tripplet coloring for numbers 1....n
+ *
+ * Therefore all pythagorean tripplets up to n are needed
+ * With these we can construct a CNF, that describes the problem:
+ *
+ * (x_a v x_b v x_c) (-x_a v -x_b v -x_c) for all a,b,c with a^2+b^2=c^2
+ *
+ * So we have at most n variables (one for each number <=n)
+ * (probably it'll be some less because some numbers won't occur in any
+ * pythagorean tripple
+ *
+ * For the number of clauses we have 2 clauses per tripplet, so:
+ * 2*F(n) where F(n) is the number of all possible pythagorean tripplets <=n
+ *
 */
 
 extern "C" {
     #include "ipasir.h"
 }
-#include <vector>
+#include <set>
 #include <tuple>
 #include <math.h>
 #include <iostream>
 
-std::vector<std::tuple<int, int, int>> generateTripplets(int n){
-	std::vector<std::tuple<int,int,int>> tripplets;
+/*
+ * Generate all pythagorean tripplets up to n
+ */
+std::set<std::tuple<int, int, int>> generateTripplets(int n){
+	std::set<std::tuple<int,int,int>> tripplets;
 	int r,x,y,z = -1;
+	//int num = 0;
 	for(int s=1;s<n;s++){
 		for(int t=1; t<n; t++){
 			r= (int) sqrt(2*s*t);
@@ -20,19 +38,25 @@ std::vector<std::tuple<int, int, int>> generateTripplets(int n){
 				if (z>n) break;
 				x = r+s;
 				y = r+t;
-				//if(!std::find(tripplets.begin(), tripplets.end(),
-				//	     std::tuple<int,int,int>(y,x,z) != tripplets.end())){
-					tripplets.push_back(std::tuple<int,int,int>(x,y,z));
+				bool is_in = tripplets.find(std::tuple<int,int,int>(y,x,z)) != tripplets.end();
+				if (!is_in){
+					//num++;
 					//std::cout<<x<<" "<<y<<" "<<z<<std::endl;
-				//}
+					tripplets.insert(std::tuple<int,int,int>(x,y,z));
+				}
 			}
 
 		}
 	}
+	//std::cout<<num<<std::endl;
 	return tripplets;
 }
 
-void buildCNF(void* solver, std::vector<std::tuple<int,int,int>> tripplets){
+
+/*
+ * Construct CNF as explained above
+ */
+void buildCNF(void* solver, std::set<std::tuple<int,int,int>> tripplets){
 	for (auto t : tripplets){
 		ipasir_add(solver, std::get<0>(t));
 		ipasir_add(solver, std::get<1>(t));
@@ -45,12 +69,11 @@ void buildCNF(void* solver, std::vector<std::tuple<int,int,int>> tripplets){
 	}
 }
 
-int sign(int x) {
-    return (x > 0) - (x < 0);
+int getColor(int x) {
+	if (x >= 1)
+	    return 1;
+	else return 0;
 }
-
-#include <iostream>
-#include <vector>
 
 int main(int argc, char **argv) {
 	std::cout << "c Using the incremental SAT solver " << ipasir_signature() << std::endl;
@@ -67,9 +90,6 @@ int main(int argc, char **argv) {
 
 	int satRes = ipasir_solve(solver);
 	
-	//std::cout << "c Number of variables: " << params[0] << std::endl;
-	//std::cout << "c Number of clauses: " << params[1] << std::endl;
-	
 	if (satRes == 20) {
 		std::cout << "c The input formula is unsatisfiable" << std::endl;
 	}
@@ -78,7 +98,7 @@ int main(int argc, char **argv) {
 		std::cout << "c The input formula is satisfiable e.g:" << std::endl;
 		for (int var = 1; var <= n; var++) {
 			int variable_val = ipasir_val(solver, var);
-			std::cout<<abs(variable_val)<<" "<<sign(variable_val)<<std::endl;
+			std::cout<<abs(variable_val)<<" "<<getColor(variable_val)<<std::endl;
 		}
 		std::cout << std::endl;
 	}
