368d4c6ee4719cc9276ab18a0b269e16a1f59b47
Andre Immig <andre.immig@students.kit.edu>
2020-05-28 20:50:25 +0200

    Working random local searcher TO-DO: Select better flipping variable

--- sheet3/localsat.cpp
+++ sheet3/localsat.cpp
@@ -15,6 +15,9 @@ struct Clause {
     int* lits;
 };
 
+
+int MAX_TRIES;
+int MAX_FLIPS;
 // Number of variables
 int numVariables;
 // Number of clauses
@@ -108,21 +111,76 @@ bool loadSatProblem(const char* filename) {
     fclose(f);
     return true;
 }
-
-void search() {
-    // search for satisfiable solution until found or forever
-    // print the solution if found
+void initRandomAssignment(int* assignment){
+     int random_bit = -1;
+     for(int i=0; i<numVariables;i++){
+         random_bit = rand() % 2;
+         if (random_bit == 0){
+             assignment[i] = -(i+1);
+         }
+         else{
+             assignment[i] = i+1;
+         }
+     }
 }
 
+bool checkClause(int* assignment, const Clause& cls){
+     for (int i=0; i<cls.numLits;i++){
+         //printf("assig[i]:%d  cls[i]%d\n",assignment[abs(cls.lits[i])-1], cls.lits[i]);
+         if(assignment[abs(cls.lits[i])-1] == cls.lits[i]) return true;
+     }
+     return false;
+}
+
+int getFlipLiteral(){
+     return rand() % (numVariables)+1;
+}
+
+bool search() {
+     int *assignment = new int[numVariables];
+     for (int i=1; i<= MAX_TRIES; i++){
+         initRandomAssignment(assignment);
+         for (int j=1; j<=MAX_FLIPS; j++){
+             bool sat = true;
+             for (int c=0; c< numClauses; c++){
+                  sat = sat && checkClause(assignment,clauses[c]);
+             }
+             if (sat){
+                  printf("s SATISFIABLE\n");
+                  printf("v ");
+                  for (int u=0; u<numVariables; u++){
+                      printf("%d ",assignment[u]);
+                  }
+                  printf("0\n");
+                  return true;
+             }
+             else{
+                 int l = getFlipLiteral();
+                 //printf("now flipping literal%d\n", l);
+                 assignment[l] *= -1;
+             }
+         }
+         //printf("------------\n");
+     }
+     return false;
+}
 
 int main(int argc, char** argv) {
     printf("c This is <your-name>'s local search satisfiability solver\n");
-    printf("c USAGE: ./<your-name> <cnf-formula-in-dimacs-format>\n");
+    printf("c USAGE: ./<your-name> <cnf-formula-in-dimacs-format> <seed> <MAX_TRIES> <MAX_FLIPS>\n");
+    if (argc != 5){
+       printf("Incorrect number of arguments specified");
+       return 1;
+    }
     if (!loadSatProblem(argv[1])) {
         printf("ERROR: problem not loaded\n");
         return 1;
     }
-    search();
-    // if your program gets to this line, it solved the problem.
-    printf("s SATISFIABLE\nv ");
+    MAX_TRIES = atoi(argv[3]);
+    MAX_FLIPS = atoi(argv[4]);
+    srand (atoi(argv[2]));
+    if(!search()){
+       printf("c No solution found in %d tries & %d flips\n", MAX_TRIES, MAX_FLIPS);
+       printf("s UNKNOWN");
+    }
 }
